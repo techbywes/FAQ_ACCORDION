29f2f0164bc3368bd6dfc7386ec4d9a66009709e
techbywes <dhspotless@gmail.com>
2023-05-02 09:19:25 +0100

    update on fonts

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   const [showModal2, setShowModal2] = useState(false);
   const [showModal3, setShowModal3] = useState(false);
   const [showModal4, setShowModal4] = useState(false);
+  const [showModal5, setShowModal5] = useState(false);
 
   const handleClick1 = () => {
     setShowModal1(!showModal1);
@@ -22,10 +23,13 @@ function App() {
   const handleClick4 = () => {
     setShowModal4(!showModal4);
   };
+  const handleClick5 = () => {
+    setShowModal5(!showModal5);
+  };
 
   return (
     <div className="container">
-      <h2 className="faq">FAQ ACCORDION</h2>
+      <h2 className="faq">- FAQ -</h2>
       <div className="App">
         <button onClick={handleClick1}>
           Why Buy An Electric Bike{" "}
@@ -103,11 +107,35 @@ function App() {
         )}
 
         <button onClick={handleClick4}>
-          When Will My Bike Be Delivered{" "}
+        What If There Is A Problem With My Bike{" "}
           <span> {showModal4 === true ? "-" : "+"} </span>
         </button>
         {showModal4 === true && (
           <div className="modul_board" onClick={handleClick4}>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero,
+              deleniti. Eos non voluptatibus ratione doloribus odio dolores
+              nulla quo neque vel in totam, ipsum deleniti inventore obcaecati
+              pariatur doloremque enim. Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Libero, deleniti. Eos non voluptatibus ratione
+              doloribus odio dolores nulla quo neque vel in totam, ipsum
+              deleniti inventore obcaecati pariatur doloremque enim. Lorem ipsum
+              dolor sit amet consectetur adipisicing elit. Libero, deleniti. Eos
+              non voluptatibus ratione doloribus odio dolores nulla quo neque
+              vel in totam, ipsum deleniti inventore obcaecati pariatur
+              doloremque enim. Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Libero, deleniti. Eos non voluptatibus ratione
+              doloribus odio dolores nulla quo neque vel in totam, ipsum
+              deleniti inventore obcaecati pariatur doloremque enim.
+            </p>
+          </div>
+        )}
+        <button onClick={handleClick4}>
+        Care And Maintenance{" "}
+          <span> {showModal5 === true ? "-" : "+"} </span>
+        </button>
+        {showModal5 === true && (
+          <div className="modul_board" onClick={handleClick5}>
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero,
               deleniti. Eos non voluptatibus ratione doloribus odio dolores
